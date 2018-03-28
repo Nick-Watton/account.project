918034ad95df4ce6aca46dcdafe4758e66d8707e
Nick-Watton <nick.watton@gmx.com>
2018-03-28 09:37:14 +0100

    Written test for account count, moved test to correct folder

--- src/test/java/account/project/ServiceTest.java
+++ src/test/java/account/project/ServiceTest.java
@@ -9,8 +9,9 @@ public class ServiceTest
 	public void test() throws Exception
 	{
 		Service service = new Service();
-		String jsonconvert = "{\"0\":{\"accountNumber\":0,\"lastName\":\"Moore\",\"firstName\":\"Matt\"},\"1\":{\"accountNumber\":1,\"lastName\":\"Watton\",\"firstName\":\"Nick\"},\"2\":{\"accountNumber\":2,\"lastName\":\"Biswas\",\"firstName\":\"Anirban\"},\"3\":{\"accountNumber\":3,\"lastName\":\"McGill\",\"firstName\":\"Andrew\"}}";
-		String jsonmap;
+		String jsonConvert = "{\"0\":{\"accountNumber\":0,\"lastName\":\"Moore\",\"firstName\":\"Matt\"},\"1\":{\"accountNumber\":1,\"lastName\":\"Watton\",\"firstName\":\"Nick\"},\"2\":{\"accountNumber\":2,\"lastName\":\"Biswas\",\"firstName\":\"Anirban\"},\"3\":{\"accountNumber\":3,\"lastName\":\"McGill\",\"firstName\":\"Andrew\"}}";
+		String jsonMap;
+		int acCount;
 		
 		service.addAccount("Matt", "Moore", 0);
 		assertTrue(service.retrieveAccount(0).getAccountNumber() == 0);
@@ -20,12 +21,20 @@ public class ServiceTest
 		assertTrue(service.retrieveAccount(2).getAccountNumber() == 2);
 		service.addAccount("Andrew", "McGill", 3);
 		assertTrue(service.retrieveAccount(3).getAccountNumber() == 3);
+		service.addAccount("Nick", "Watton", 4);
+		assertTrue(service.retrieveAccount(4).getAccountNumber() == 4);
 		
-		jsonmap = service.translateAccount();
-		assertEquals(jsonmap, jsonconvert);
+		jsonMap = service.translateAccount();
+		assertEquals(jsonMap, jsonConvert);
+		service.removeAccount(4);
 		service.removeAccount(3);
 		service.removeAccount(2);
 		service.removeAccount(1);
 		service.removeAccount(0);
+		
+		acCount = Service.countAccount("Nick");
+		assertEquals(acCount, 2);
+		
+		
 	}
 }
