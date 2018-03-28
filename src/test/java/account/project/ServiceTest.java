d1b583e35d1f4158ba17d27f59386d4d09b5de75
Nick-Watton <nick.watton@gmx.com>
2018-03-28 10:33:32 +0100

    Refactored test and main code

--- src/test/java/account/project/ServiceTest.java
+++ src/test/java/account/project/ServiceTest.java
@@ -2,42 +2,54 @@ package account.project;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
-public class ServiceTest
-{
-	@Test
-	public void test() throws Exception
-	{
-		Service service = new Service();
-		String jsonConvert = "{\"0\":{\"firstName\":\"Matt\",\"lastName\":\"Moore\",\"accountNumber\":0},\"1\":{\"firstName\":\"Nick\",\"lastName\":\"Watton\",\"accountNumber\":1},\"2\":{\"firstName\":\"Anirban\",\"lastName\":\"Biswas\",\"accountNumber\":2},\"3\":{\"firstName\":\"Andrew\",\"lastName\":\"McGill\",\"accountNumber\":3},\"4\":{\"firstName\":\"Nick\",\"lastName\":\"Watton\",\"accountNumber\":4}}"; 
-		String jsonMap;
-		int acCount;
-		
+public class ServiceTest {
+
+	Service service = new Service();
+
+	@Before
+	public void testSetUp() {
 		service.addAccount("Matt", "Moore", 0);
-		assertTrue(service.retrieveAccount(0).getAccountNumber() == 0);
 		service.addAccount("Nick", "Watton", 1);
-		assertTrue(service.retrieveAccount(1).getAccountNumber() == 1);
 		service.addAccount("Anirban", "Biswas", 2);
-		assertTrue(service.retrieveAccount(2).getAccountNumber() == 2);
 		service.addAccount("Andrew", "McGill", 3);
-		assertTrue(service.retrieveAccount(3).getAccountNumber() == 3);
 		service.addAccount("Nick", "Watton", 4);
-		assertTrue(service.retrieveAccount(4).getAccountNumber() == 4);
 		
-		jsonMap = service.translateAccount();
-		assertEquals(jsonMap, jsonConvert);
-		
-		acCount = service.countAccount("Nick");
-		assertEquals(2, acCount);
-		
+	}
+	
+	@After
+	public void testCleanUp() {
 		service.removeAccount(4);
 		service.removeAccount(3);
 		service.removeAccount(2);
 		service.removeAccount(1);
-		service.removeAccount(0);
-		
-
-		
-		
+		service.removeAccount(0);	
+	}
+	
+	@Test
+	public void checkElementsExist() {
+		assertTrue(service.retrieveAccount(0).getAccountNumber() == 0);
+		assertTrue(service.retrieveAccount(1).getAccountNumber() == 1);
+		assertTrue(service.retrieveAccount(2).getAccountNumber() == 2);
+		assertTrue(service.retrieveAccount(3).getAccountNumber() == 3);
+		assertTrue(service.retrieveAccount(4).getAccountNumber() == 4);
+	}
+	
+	@Test
+	public void translateToJSONFromMap() throws Exception {
+		String jsonConvert = "{\"0\":{\"firstName\":\"Matt\",\"lastName\":\"Moore\",\"accountNumber\":0},\"1\":{\"firstName\":\"Nick\",\"lastName\":\"Watton\",\"accountNumber\":1},\"2\":{\"firstName\":\"Anirban\",\"lastName\":\"Biswas\",\"accountNumber\":2},\"3\":{\"firstName\":\"Andrew\",\"lastName\":\"McGill\",\"accountNumber\":3},\"4\":{\"firstName\":\"Nick\",\"lastName\":\"Watton\",\"accountNumber\":4}}"; 
+		String jsonMap;
+		jsonMap = service.translateAccount();
+		assertEquals(jsonMap, jsonConvert);
+	
+	}
+	
+	@Test
+	public void countAccounts() {
+		int acCount;
+		acCount = service.countAccount("Nick");
+		assertEquals(2, acCount);
 	}
 }
